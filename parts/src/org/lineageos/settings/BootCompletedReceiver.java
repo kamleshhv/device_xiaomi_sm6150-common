3a6b670e4c81e8ddebf97284b612615703ec9522
kamleshhv <kamleshvansajalia@gmail.com>
2023-12-25 10:05:10 +0530

    Revert "sm6150-common: parts: Introduce Dolby Atmos"
    This reverts commit 532e08c7b463745743b0b23fd08ba71b8b2f959e.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,7 +25,6 @@ import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.dolby.DolbyUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 
@@ -44,10 +43,14 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.onBootCompleted(context);
 
         // Dirac
+<<<<<<< HEAD
         // DiracUtils.onBootCompleted(context);
 
         // Dolby Atmos
         DolbyUtils.getInstance(context);
+=======
+        DiracUtils.onBootCompleted(context);
+>>>>>>> parent of 532e08c (sm6150-common: parts: Introduce Dolby Atmos)
 
         // Thermal Profiles
         ThermalUtils.startService(context);
