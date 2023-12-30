a180b9a756d51437b4dd0e0476355815efe27e85
kamleshhv <kamleshvansajalia@gmail.com>
2023-12-30 18:18:00 +0530

    Revert "sm6150-common: parts: Set proper summary for dolby settings"
    This reverts commit ce2f1eb3557c78aa998593ce20f17dd40760f5b7.

--- parts/src/org/lineageos/settings/dolby/DolbyUtils.java
+++ parts/src/org/lineageos/settings/dolby/DolbyUtils.java
@@ -39,8 +39,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 >>>>>>> b194474 (marble: parts: Introduce Dolby Atmos)
 
-import org.lineageos.settings.R;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -297,16 +295,6 @@ public final class DolbyUtils {
 =======
         Log.i(TAG, "getProfile: " + profile);
         return profile;
-    }
-
-    public String getProfileName() {
-        String profile = Integer.toString(mDolbyAtmos.getProfile());
-        List<String> profiles = Arrays.asList(mContext.getResources().getStringArray(
-                R.array.dolby_profile_values));
-        int profileIndex = profiles.indexOf(profile);
-        Log.i(TAG, "getProfileAsString: profile=" + profile + " index=" + profileIndex);
-        return profileIndex == -1 ? null : mContext.getResources().getStringArray(
-                R.array.dolby_profile_entries)[profileIndex];
     }
 
     public void setPreset(String preset) {
